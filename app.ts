17f3b68eef499771c212a2fbbb5301fa2cb57be5
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-10-05 12:03:15 +0700

    [Feat] supabase client and field get all

--- app.ts
+++ app.ts
@@ -2,15 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import fieldRouter from './routes/FieldRouter';
 import env from 'dotenv';
-import { createClient } from '@supabase/supabase-js';
 
 env.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 app.use(cors());
 
 app.use('/', fieldRouter);
--- routes/FieldRouter.ts
+++ routes/FieldRouter.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import supabase from "../supabaseClient";
+
 var fieldRouter = express.Router();
 
 /* GET users listing. */
-fieldRouter.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+fieldRouter.get('/', async function(req, res, next) {
+    let { data: Field, error } = await supabase
+    .from('Field')
+    .select('*');
+    
+    if (error) {
+        return res.status(400).json({error: error.message});
+    } 
+
+    res.json(Field);
 });
 
 export default fieldRouter;

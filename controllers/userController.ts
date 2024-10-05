14130b1b416ff7bc308842e280d3777989284ba8
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-10-05 18:15:19 +0700

    [Fix] Return user id on regist

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -21,7 +21,7 @@ export const createNewUser = async (req: Request, res: Response) => {
     const { email, name, password, location } = req.body;
     const newUser = await createUser({ email, name, password, location });
     
-    return res.status(201).json({ message: 'Account created successfully' });
+    return res.status(201).json({ userId: newUser.id });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
